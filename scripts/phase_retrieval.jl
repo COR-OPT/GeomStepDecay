42b03617f0c8c6506cc08cc41b684a3b8fbf9b6f
vchariso <vharisop@gmail.com>
2022-05-28 16:31:19 -0400

    Bump up version, add phase retrieval callbacks

---
+++ scripts/phase_retrieval.jl
@@ -0,0 +1,74 @@
+import LinearAlgebra: norm, normalize
+import Distributions
+
+import GeomStepDecay
+
+struct PhaseRetrievalProblem <: GeomStepDecay.OptProblem
+  A::Distributions.Distribution
+  x::Vector{Float64}
+  pfail::Float64
+end
+
+function generate_problem(D::Distributions.Distribution, d::Int, pfail::Float64 = 0.0)
+  return PhaseRetrievalProblem(D, normalize(randn(d)), pfail)
+end
+
+function distance_to_solution(
+  problem::PhaseRetrievalProblem,
+  x::Vector{Float64},
+)
+  return min(
+    norm(x - problem.x),
+    norm(x + problem.x),
+  )
+end
+
+"""
+  generate_samples(problem::PhaseRetrievalProblem, num_samples::Int) -> (vectors, measurements)
+
+Generate `num_samples` measurement vectors and measurements for a
+`PhaseRetrievalProblem`.
+
+Returns:
+- `vectors::Matrix{Float64}`: a `num_samples × d` matrix of measurement vectors.
+- `measurements::Vector{Float64}`: a vector holding the `num_samples` resulting measurements.
+"""
+function generate_samples(
+  problem::PhaseRetrievalProblem,
+  num_samples::Int,
+)
+  d = length(problem.x)
+  p = problem.pfail
+  vectors = Matrix(rand(problem.A, num_samples)')
+  measurements = (vectors * problem.x) .^ 2
+  # Replace a `p` fraction with large noise.
+  replace!(
+    x -> (rand() <= p) ? 10 * randn().^2 : x,
+    measurements,
+  )
+  return vectors, measurements
+end
+
+"""
+  subgradient_step(problem::PhaseRetrievalProblem, x::Vector{Float64}, step_size::Float64;
+                   batch_size::Int = 1)
+
+Take one step of the subgradient method for `problem` starting at `x` with a
+given `step_size`. Use a batch of size `batch_size` when computing the
+subgradient.
+"""
+function subgradient_step(
+  problem::PhaseRetrievalProblem,
+  x::Vector{Float64},
+  step_size::Float64;
+  batch_size::Int = 1,
+)
+  A, y = generate_samples(problem, batch_size)
+  # Subgradient for given batch.
+  subgrad = 2 * A' * (sign.((A * x).^2 .- y) .* A * x)
+  return x - step_size * subgrad
+end
+
+# Callback function for distance to solution.
+distance_callback(problem::PhaseRetrievalProblem, x::Vector{Float64}, _) =
+  distance_to_solution(problem, x)
--- src/GeomStepDecay.jl
+++ src/GeomStepDecay.jl
@@ -9,62 +9,104 @@ using Statistics
 
 abstract type OptProblem end
 
+"""
+  mba_template(problem::OptProblem, x₀::Vector{Float64}, step_size::Float64, num_iterations::Int, is_conv::Bool, prox_step::Function; stop_condition::Function = nothing)
+
+A template for running the MBA algorithm.
+
+Arguments:
+- `problem::OptProblem`: The problem to solve.
+- `x₀::Vector{Float64}`: The initial guess for the algorithm.
+- `step_size::Float64`: The step size for each iteration.
+- `num_iterations::Int`: The total iteration budget.
+- `is_conv::Bool`: If set, the function returns the average iterate.
+- `prox_step::Function`: A callable implementing a proximal step of the model.
+- `stop_condition::Function = nothing`: A callable implementing a stopping criterion.
+
+Notes:
+1. The `prox_step` callable should accept the problem, current iterate and step size.
+2. The `stop_condition` callable should accept the problem, current iterate and iteration index.
+"""
 function mba_template(
-  problem::OptProblem, x₀::Vector{Float64}, step_size::Float64,
-  num_iterations::Int, is_conv::Bool, prox_step::Function;
-  stop_condition::Function = nothing
+  problem::OptProblem,
+  x₀::Vector{Float64},
+  step_size::Float64,
+  num_iterations::Int,
+  is_conv::Bool,
+  prox_step::Function;
+  stop_condition::Union{Nothing,Function}=nothing,
 )
   # If no condition provided, set to always false.
-  stop_condition = (stop_condition == nothing) ? ((_...) -> false) : stop_condition
-  if is_conv
-    running_sum = x₀[:]
-    for k in 1:num_iterations
-      x₀ = prox_step(problem, x₀, step_size)
-      running_sum .+= x₀[:]
-      if stop_condition(problem, x₀, k)
-        @debug "Stopping early at iteration: $(k)"
-        return (1 / (k + 1)) * running_sum
-      end
+  stop_condition =
+    (stop_condition === nothing) ? ((_...) -> false) : stop_condition
+  running_avg = x₀[:]
+  stop_time = is_conv ? num_iterations : rand(0:num_iterations)
+  for k in 1:stop_time
+    x₀ = prox_step(problem, x₀, step_size)
+    if is_conv
+      running_avg = (k / (k + 1)) .* running_avg .+ (1 / (k + 1)) .* x₀
+    else
+      running_avg = x₀[:]
     end
-    return (1 / (num_iterations + 1)) * running_sum
-  else
-    stop_time = rand(0:num_iterations)
-    for k in 1:stop_time
-      x₀ = prox_step(problem, x₀, step_size)
-      if stop_condition(problem, x₀, k)
-        @debug "Stopping early at iteration: $(k)"
-        return x₀
-      end
+    if stop_condition(problem, x₀, k)
+      @debug "Stopping early at iteration: $(k)"
+      return x₀
     end
-    return x₀
   end
+  return running_avg
 end
 
+"""
+  rmba_template(problem::OptProblem, x₀::Vector{Float64}, initial_step_size::Float64,
+                outer_iterations::Int, inner_iterations::Int, is_conv::Bool,
+                prox_step::Function, callback::Function, stop_condition::Function = nothing) -> (x, callback_results)
 
+A template for running the restarted MBA (RMBA) algorithm, which invokes
+MBA algorithm `outer_iterations` times for `inner_iterations` each using a
+geometrically decreasing step size in each outer iteration.
+
+Here, `callback(problem, x, it)` is a function invoked in each outer iteration.
+"""
 function rmba_template(
-  problem::OptProblem, x₀::Vector{Float64}, initial_step_size::Float64,
-  outer_iterations::Int, inner_iterations::Int, is_conv::Bool,
-  prox_step::Function, callback::Function;
-  stop_condition::Function = nothing
+  problem::OptProblem,
+  x₀::Vector{Float64},
+  initial_step_size::Float64,
+  outer_iterations::Int,
+  inner_iterations::Int,
+  is_conv::Bool,
+  prox_step::Function,
+  callback::Function;
+  stop_condition=nothing,
 )
   callback_results = zeros(outer_iterations)
   for t in 1:outer_iterations
-    step = initial_step_size * 2^(-(t - 1))
-    x₀ = mba(problem, x₀[:], step, inner_iterations, is_conv, prox_step,
-             stop_condition=stop_condition)
+    step = initial_step_size * 2.0^(-(t - 1))
+    x₀ = mba_template(
+      problem,
+      x₀[:],
+      step,
+      inner_iterations,
+      is_conv,
+      prox_step;
+      stop_condition=stop_condition,
+    )
     callback_results[t] = callback(problem, x₀, t)
   end
   return x₀, callback_results
 end
 
-
 function pmba_template(
-  problem::OptProblem, x₀::Vector{Float64}, step_size::Float64,
-  prox_penalty::Float64, num_iterations::Int, prox_step::Function;
-  stop_condition::Function = nothing
+  problem::OptProblem,
+  x₀::Vector{Float64},
+  step_size::Float64,
+  prox_penalty::Float64,
+  num_iterations::Int,
+  prox_step::Function;
+  stop_condition::Function=nothing,
 )
   # If stop_condition is unset, make it always false.
-  stop_condition = (stop_condition == nothing) ? ((_...) -> false) : stop_condition
+  stop_condition =
+    (stop_condition === nothing) ? ((_...) -> false) : stop_condition
   # Proximal center for the updates.
   x_base = x₀[:]
   stop_time = rand(0:num_iterations)
@@ -73,9 +115,11 @@ function pmba_template(
     # Including the proximal penalty is equivalent to evaluating
     # the proximal operator at a nearby point with a slightly different
     # proximal step.
-    x₀ = prox_step(problem,
-                   weight * (step_size * x₀ + prox_penalty * step_size * x_base),
-                   weight * step_size)
+    x₀ = prox_step(
+      problem,
+      weight * (step_size * x₀ + prox_penalty * step_size * x_base),
+      weight * step_size,
+    )
     if stop_condition(problem, x₀, k)
       @debug "Stopping early at iteration: $(k)"
       return x₀
@@ -84,25 +128,34 @@ function pmba_template(
   return x₀
 end
 
-
 function epmba_template(
-  problem::OptProblem, x₀::Vector{Float64}, step_size::Float64,
-  prox_penalty::Float64, ensemble_radius::Float64, num_repeats::Int,
-  num_iterations::Int, prox_step::Function;
-  stop_condition::Function = nothing
+  problem::OptProblem,
+  x₀::Vector{Float64},
+  step_size::Float64,
+  prox_penalty::Float64,
+  ensemble_radius::Float64,
+  num_repeats::Int,
+  num_iterations::Int,
+  prox_step::Function;
+  stop_condition::Function=nothing,
 )
   ϵ = ensemble_radius
   Xs = zeros(length(x₀), num_repeats)
   for i in 1:num_repeats
-    Xs[:, i] = pmba_template(problem, x₀[:], step_size, prox_penalty,
-                             num_iterations, prox_step,
-                             stop_condition=stop_condition)
+    Xs[:, i] = pmba_template(
+      problem,
+      x₀[:],
+      step_size,
+      prox_penalty,
+      num_iterations,
+      prox_step;
+      stop_condition=stop_condition,
+    )
   end
   pairwise_dists = pairwise_distance(Xs)
-  most_idx = argmax(vec(sum(pairwise_dists .<= 2 * ϵ, dims=2)))
+  most_idx = argmax(vec(sum(pairwise_dists .<= 2 * ϵ; dims=2)))
   return Xs[:, most_idx]
 end
-
 
 """
   rpmba_template(problem::OptProblem, x₀::Vector{Float64}, initial_step_size::Float64,
@@ -121,24 +174,38 @@ On the other hand, `callback(problem, current_point, outer_iteration_index)`
 should produce a single scalar (for example, the distance to the solution).
 """
 function rpmba_template(
-  problem::OptProblem, x₀::Vector{Float64}, initial_step_size::Float64,
-  initial_prox_penalty::Float64, initial_ensemble_radius::Float64,
-  inner_iterations::Int, outer_iterations::Int, num_repeats::Int,
-  prox_step::Function, callback::Function;
-  stop_condition::Function = nothing
+  problem::OptProblem,
+  x₀::Vector{Float64},
+  initial_step_size::Float64,
+  initial_prox_penalty::Float64,
+  initial_ensemble_radius::Float64,
+  inner_iterations::Int,
+  outer_iterations::Int,
+  num_repeats::Int,
+  prox_step::Function,
+  callback::Function;
+  stop_condition::Function=nothing,
 )
   callback_results = zeros(outer_iterations)
   for t in 0:(outer_iterations - 1)
     ρ = 2^t * initial_prox_penalty
     α = 2^(-t) * initial_step_size
     ϵ = 2^(-t) * initial_ensemble_radius
-    x₀ = epmba_template(problem, x₀[:], α, ρ, ϵ, num_repeats, inner_iterations,
-                        prox_step, stop_condition=stop_condition)
-    callback_results[t+1] = callback(problem, x₀, t)
+    x₀ = epmba_template(
+      problem,
+      x₀[:],
+      α,
+      ρ,
+      ϵ,
+      num_repeats,
+      inner_iterations,
+      prox_step;
+      stop_condition=stop_condition,
+    )
+    callback_results[t + 1] = callback(problem, x₀, t)
   end
   return x₀, callback_results
 end
-
 
 """
   pairwise_distance(V::Matrix{Float64})
@@ -147,7 +214,7 @@ Given a `d × k` matrix, return a `k × k` matrix such that the (i, j)
 element holds the distance between `V[:, i]` and `V[:, j]`.
 """
 function pairwise_distance(V::Matrix{Float64})
-  col_norms = sum(V.^2, dims=1)
+  col_norms = sum(V .^ 2; dims=1)
   return col_norms .+ col_norms' - 2 * V'V
 end
 
