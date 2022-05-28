7a98e79005822a94966097805a84426ac6c74649
vchariso <vharisop@gmail.com>
2022-05-28 18:43:26 -0400

    Add phase retrieval experiment with batch size

---
+++ scripts/phase_retrieval_experiment.jl
@@ -0,0 +1,90 @@
+using ArgParse
+using CSV
+using DataFrames
+using Distributions
+using LinearAlgebra
+using Random
+using Printf
+using PyPlot
+
+import GeomStepDecay
+
+# enable latex printing
+rc("text", usetex=true)
+
+include("phase_retrieval.jl")
+
+LBLUE="#519cc8"
+MBLUE="#1d5996"
+HBLUE="#908cc0"
+TTRED="#ca3542"
+
+struct TrialResult
+  dist_real::Float64
+  dist_calc::Float64
+  iter_ind::Int
+  sample_ind::Int
+end
+
+function main(d, pfail, δ, ϵ_stop=1e-15)
+  μ = 1 - 2 * pfail
+  L = sqrt(d)
+  η = 1.0
+  δ_fail = 1 / 3
+  ϵ = 1e-5
+  T = trunc(Int, ceil(log2(2 * δ / ϵ)))
+  K = trunc(Int, T * (L / (δ_fail * μ))^2)
+  @info "T = $T, K = $K, d = $d"
+  R = sqrt(δ) * μ
+  α₀ = (R / L) * (1 / sqrt(K + 1))
+  batch_size = trunc(Int, sqrt(d))
+  callback(problem::PhaseRetrievalProblem, x::Vector{Float64}, t::Int) =
+    (dist_real = distance_to_solution(problem, x),
+     dist_calc = 2.0^(-t) * R,
+     iter_ind = t * K * batch_size)
+  # Standard normal distribution
+  D = Distributions.MultivariateNormal(d, 1.0)
+  problem = generate_problem(D, pfail)
+  step_fn = (p, x, α) -> subgradient_step(p, x, α, batch_size=batch_size)
+  x₀ = problem.x + δ * normalize(randn(d))
+  _, callback_results = GeomStepDecay.rmba_template(
+    problem,
+    x₀,
+    α₀,
+    5 * T,
+    K,
+    false,
+    step_fn,
+    callback,
+    stop_condition = (p, x, _) -> (distance_to_solution(p, x) ≤ ϵ_stop),
+  )
+  CSV.write(
+    "phase_retrieval_$(d)_$(@sprintf("%.2f", pfail)).csv",
+    DataFrame(callback_results),
+  )
+end
+
+settings = ArgParseSettings(
+  description="Run the stochastic subgradient algorithm on phase retrieval.",
+)
+@add_arg_table! settings begin
+  "--d"
+    help = "Problem dimension"
+    arg_type = Int
+  "--p"
+    help = "Corruption probability"
+    arg_type = Float64
+    default = 0.2
+  "--delta"
+    help = "Initial normalized distance"
+    arg_type = Float64
+    default = 0.25
+  "--seed"
+    help = "The random generator seed."
+    arg_type = Int
+    default = 999
+end
+
+parsed = parse_args(settings)
+Random.seed!(parsed["seed"])
+main(parsed["d"], parsed["p"], parsed["delta"])
